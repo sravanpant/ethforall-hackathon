4bc9fc6bfa467afbdc7ebb6faeabc0c7ac470332
armanthepythonguy <armanityours@gmail.com>
2023-02-05 10:46:45 +0530

    configuring solc

--- trading-engine/main.go
+++ trading-engine/main.go
@@ -15,7 +15,7 @@ import(
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
-	store "MarketPlace"
+	store "MMarketPlace"
 )
 
 type Bid struct{
